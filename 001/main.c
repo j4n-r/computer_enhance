b6c1b766800fbcd97c4ce05954564f94329acf83
j4n-r <jan.ruegge@gmail.com>
2025-02-18 20:49:53 +0100

    finished assignment

--- 001/main.c
+++ 001/main.c
@@ -1,74 +1,101 @@
-
 #include <assert.h>
 #include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+const char* MOD_W0_NAMES[] = {"al", "cl", "dl", "bl", "ah", "ch", "dh", "bh"};
 
-#define OP_MOV 0b10001
+enum op_c {
+    mov = 0b100010,
+};
+const char* MOD_W1_NAMES[] = {"ax", "cx", "dx", "bx", "sp", "bp", "si", "di"};
 
-int openFile(char *fileName, unsigned char *buffer) {
-	FILE *file = fopen(fileName, "r");
-	if (!file) {
-		perror("Error opening file");
-		return EXIT_FAILURE;
-	}
-	assert(fseek(file, 0, SEEK_END) == 0);
-	long fileSize = ftell(file);
-	assert(fileSize != -1);
-	rewind(file);
+unsigned char* openFile(const char* fileName, size_t* fileSize) {
+    FILE* file = fopen(fileName, "rb");
+    if (!file) {
+        perror("Error opening file");
+        return NULL;
+    }
 
-	buffer = malloc(fileSize);
-	assert(buffer != NULL);
+    assert(fseek(file, 0, SEEK_END) == 0);
+    long size = ftell(file);
+    assert(size != -1);
+    rewind(file);
 
-	size_t bytesRead = fread(file, 1, fileSize, file);
-	if (bytesRead != fileSize) {
-		perror("Error readind file");
-		return EXIT_FAILURE;
-	}
-	return EXIT_SUCCESS;
+    unsigned char* buffer = (unsigned char*)malloc(size);
+    if (!buffer) {
+        perror("Memory allocation failed");
+        fclose(file);
+        return NULL;
+    }
+
+    size_t bytesRead = fread(buffer, 1, size, file);
+    if (bytesRead != (size_t)size) {
+        perror("Error reading file");
+        free(buffer);
+        fclose(file);
+        return NULL;
+    }
+
+    fclose(file);
+    *fileSize = size;
+    return buffer;
 }
 
-int decodeASM(unsigned char *buffer) {
-	char outputBuf[10000];
-	char *registers[2][8] = {
-	    {"AL", "CL", "DL", "BL", "AH", "CH", "DH",
-	     "BH"}, // W = 0 (8-bit registers)
-	    {"AX", "CX", "DX", "BX", "SP", "BP", "SI", "DI"}
-	    // W = 1 (16-bit registers)
-	};
+int decodeASM(unsigned char* buffer, size_t size) {
 
-	for (uint8_t i = 0; i < sizeof(buffer); i++) {
-		uint8_t byte = buffer[i];
-		int op = byte >> 2;
-		if (op == OP_MOV) {
-			strcpy("mov ", outputBuf);
-		}
-		int w = byte & 1 ? 1 : 0;
-		int b = byte >> 1 & 1 ? 1 : 0;
+    FILE* file = fopen("output.asm", "w");
+    fprintf(file, "bits 16\n\n");
 
-		uint8_t byte2 = buffer[i + 1];
-		uint8_t mod = byte2 >> 5;
-		for (uint8_t i = 0; i < 8; i++) {
-			if (i == mod) {
-				if (!w) {
-					strcpy(registers[0][i], outputBuf);
-				} else {
-					strcpy(registers[1][i], outputBuf);
-				}
-			}
-		}
-	}
-	printf("%s", outputBuf);
+    for (size_t i = 0; i < size; i += 2) {
+        int w = buffer[i] & 1;
+        int d = (buffer[i] >> 1) & 1;
+        enum op_c op = buffer[i] >> 2;
+
+        switch (op) {
+        case mov:
+            printf("mov ");
+            fprintf(file, "mov ");
+            break;
+        default:
+            printf("unknown op code");
+        }
+        int reg = (buffer[i + 1] >> 3) & 0b111;
+        int r_m = buffer[i + 1] & 0b111;
+
+        int src = 0;
+        int dst = 0;
+        if (d == 0) {
+            src = reg;
+            dst = r_m;
+        } else {
+            src = r_m;
+            dst = reg;
+        }
+        if (w == 0) {
+            printf("%s, %s\n", MOD_W0_NAMES[dst], MOD_W0_NAMES[src]);
+            fprintf(file, "%s, %s\n", MOD_W0_NAMES[dst], MOD_W0_NAMES[src]);
+        } else {
+            printf("%s, %s\n", MOD_W1_NAMES[dst], MOD_W1_NAMES[src]);
+            fprintf(file, "%s, %s\n", MOD_W1_NAMES[dst], MOD_W1_NAMES[src]);
+        }
+    }
+
+    fclose(file);
+    return EXIT_SUCCESS;
 }
 
 int main() {
-	unsigned char *buffer;
-	if (openFile("listing37", buffer)) {
-		perror("Error reading file main");
-		return EXIT_FAILURE;
-	}
-	decodeASM(buffer);
-	return EXIT_SUCCESS;
+    size_t fileSize = 0;
+    unsigned char* buffer = openFile("listing38", &fileSize);
+    if (buffer == NULL) {
+        fprintf(stderr, "Error reading file in main\n");
+        return EXIT_FAILURE;
+    }
+
+    decodeASM(buffer, fileSize);
+
+    free(buffer);
+    return EXIT_SUCCESS;
 }
